22c62b8d04038a3411669cda6ed34656e9299f82
Pedro Cattel <pedrocattel@hotmail.com>
2017-03-29 18:08:56 -0300

    Versao Final ex 2

--- pratica2/ex2/TesteHeranca/src/testeheranca/GerenteDeVendas.java
+++ pratica2/ex2/TesteHeranca/src/testeheranca/GerenteDeVendas.java
@@ -3,90 +3,104 @@ package testeheranca;
 
 public class GerenteDeVendas {
     
-    private String nome;
+     private String nomeCompleto;
     private String cpf;
-    private String sobrenome;
-    private int salarioBase;
     private int registro;
-    private int horaExtra;
+    private double salarioBase;
+    private static double taxaComissao = 0.03;
+    private double horaExtra;
     private double totalVendas;
-    private double taxaComissao; //static
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    private int vendedores;
+    private String senha;
+    
+    public GerenteDeVendas (String n, String c, int reg, double salario, double hora) {
+        this.setNome(n);
+        this.setCpf(c);
+        this.setRegistro(reg);
+        this.setSalarioBase(salario);
+        this.setHoraExtra(hora);
     }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    
+    public String getNome () {
+        return this.nomeCompleto;
     }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+    
+    public void setNome (String nomeNovo) {
+        this.nomeCompleto = nomeNovo;
     }
-
-    public void setSalario(int salario) {
-        this.salarioBase = salario;
-    }
-
-    public void setRegistro(int registro) {
-        this.registro = registro;
-    }
-
-    public void setHoraExtra(int horaextra) {
-        this.horaExtra = horaextra;
-    }
-
-    public void settaxaComissao(int taxacomissao) {
-        this.taxaComissao = taxacomissao;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public String getCpf() {
+    
+    public String getCpf () {
         return this.cpf;
     }
-
-    public String getSobrenome() {
-        return this.sobrenome;
+    
+    public void setCpf (String cpfNovo) {
+        this.cpf = cpfNovo;
     }
-
-    public int getSalarioBase() {
-        return this.salarioBase;
-    }
-
-    public int getRegistro() {
+    
+    public int getRegistro () {
         return this.registro;
     }
-
-    public int getHoraExtra() {
+    
+    public void setRegistro (int registroNovo) {
+        this.registro = registroNovo;
+    }
+    
+    public double getHoraExtra () {
         return this.horaExtra;
     }
-
-    public int getTaxaComissao() {
-        return this.taxaComissao;
+    
+    public void setHoraExtra (double horaNova) {
+        this.horaExtra = horaNova;
     }
-
-    public int decTer() {
+    
+    public double getSalarioBase () {
         return this.salarioBase;
     }
-
-    public double comissao(double totalVendas) {
-        this.taxaComissao = totalVendas * 0.03;
-        return this.taxaComissao;
+    
+    public void setSalarioBase (double salarioNovo) {
+        this.salarioBase = salarioNovo;
     }
-
-    public int calcFerias(int salarioBase) {
-        this.salarioBase = (this.salarioBase * (1/3) + this.salarioBase);
+    
+    public static double getTaxa () {
+        return GerenteDeVendas.taxaComissao;
+    }
+    
+    public static void setTaxa (double taxaNova) {
+        GerenteDeVendas.taxaComissao = taxaNova;
+    }
+    
+    public void setSenha (String senhaNova) {
+        this.senha = senhaNova;
+    }
+    
+    public double calcularComissao () {
+        return this.totalVendas * GerenteDeVendas.taxaComissao;
+    }
+    
+    public double calcularDecimoTerceiro () {
         return this.salarioBase;
     }
-
-    public int salarioDoMes(int salarioBase, int horasExtras, int comissao) {
-        return ( this.salarioBase + horasExtras + comissao);
+    
+    public double calcularFerias () {
+        return this.salarioBase + this.salarioBase/3;
     }
-
-    public boolean contabilizarVenda(double venda) {
+    
+    public double salarioDoMes () {
+        return this.salarioBase + this.horaExtra + this.calcularComissao();
+    }
+    
+    public void exibirResumo () {
+        System.out.println("Nome: " + this.nomeCompleto);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Registro: " + this.registro);
+        System.out.println("Salário total: " + this.salarioDoMes());
+        System.out.println("Comissão: " + this.calcularComissao());
+        System.out.println("Férias: " + this.calcularFerias());
+        System.out.println("13º: " + this.calcularDecimoTerceiro());
+        System.out.println("Vendas: " + this.totalVendas);
+    }
+    
+    public boolean contabilizarVenda (double venda) {
         if (venda > 0) {
             this.totalVendas = this.totalVendas + venda;
             return true;
@@ -95,12 +109,11 @@ public class GerenteDeVendas {
         }
     }
     
-    public void gerente (String nome, String cpf, String sobrenome, int registro, int salario){
-        this.setNome(nome);
-        this.setSobrenome(sobrenome);
-        this.setCpf(cpf);
-        this.setSalario(salario);
-        this.setRegistro(registro);
-        
+    public void incrementarVendedor () {
+        this.vendedores = this.vendedores + 1;
+    }
+    
+    public void decrementarVendedor () {
+        this.vendedores = this.vendedores - 1;
     }
 }
--- pratica2/ex2/TesteHeranca/src/testeheranca/Vendedor.java
+++ pratica2/ex2/TesteHeranca/src/testeheranca/Vendedor.java
@@ -3,90 +3,109 @@ package testeheranca;
 
 public class Vendedor {
 
-    private String nome;
+   private String nomeCompleto;
     private String cpf;
-    private String sobrenome;
-    private int salarioBase;
     private int registro;
-    private int horaExtra;
+    private double salarioBase;
+    private static double taxaComissao = 0.03;
+    private double horaExtra;
     private double totalVendas;
-    private int taxaComissao; //static
-    private Gerente gerente;
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    private GerenteDeVendas gerente;
+    
+    public Vendedor (String n, String c, int reg, double salario, double hora, GerenteDeVendas ger) {
+        this.setNome(n);
+        this.setCpf(c);
+        this.setRegistro(reg);
+        this.setSalarioBase(salario);
+        this.setHoraExtra(hora);
+        this.setGerente(ger);
+        ger.incrementarVendedor();
     }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    
+    public String getNome () {
+        return this.nomeCompleto;
     }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
+    
+    public void setNome (String nomeNovo) {
+        this.nomeCompleto = nomeNovo;
     }
-
-    public void setSalario(int salario) {
-        this.salarioBase = salario;
-    }
-
-    public void setRegistro(int registro) {
-        this.registro = registro;
-    }
-
-    public void setHoraExtra(int horaextra) {
-        this.horaExtra = horaextra;
-    }
-
-    public void settaxaComissao(int taxacomissao) {
-        this.taxaComissao = taxacomissao;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public String getCpf() {
+    
+    public String getCpf () {
         return this.cpf;
     }
-
-    public String getSobrenome() {
-        return this.sobrenome;
+    
+    public void setCpf (String cpfNovo) {
+        this.cpf = cpfNovo;
     }
-
-    public int getSalarioBase() {
-        return this.salarioBase;
-    }
-
-    public int getRegistro() {
+    
+    public int getRegistro () {
         return this.registro;
     }
-
-    public int getHoraExtra() {
+    
+    public void setRegistro (int registroNovo) {
+        this.registro = registroNovo;
+    }
+    
+    public double getHoraExtra () {
         return this.horaExtra;
     }
-
-    public int getTaxaComissao() {
-        return this.taxaComissao;
+    
+    public void setHoraExtra (double horaNova) {
+        this.horaExtra = horaNova;
     }
-
-    public int decTer() {
+    
+    public double getSalarioBase () {
         return this.salarioBase;
     }
-
-    public double comissao(double totalVendas) {
-        return totalVendas * 0.03;
+    
+    public void setSalarioBase (double salarioNovo) {
+        this.salarioBase = salarioNovo;
     }
-
-    public int calcFerias(int salarioBase) {
-        this.salarioBase = (this.salarioBase * (1/3) + this.salarioBase);
+    
+    public static double getTaxa () {
+        return Vendedor.taxaComissao;
+    }
+    
+    public static void setTaxa (double taxaNova) {
+        Vendedor.taxaComissao = taxaNova;
+    }
+    
+    public String getGerente () {
+        return this.gerente.getNome();
+    }
+    
+    public void setGerente (GerenteDeVendas gerenteNovo) {
+        this.gerente = gerenteNovo;
+    }
+    
+    public double calcularComissao () {
+        return this.totalVendas * Vendedor.taxaComissao;
+    }
+    
+    public double calcularDecimoTerceiro () {
         return this.salarioBase;
     }
-
-    public int salarioDoMes(int salarioBase, int horasExtras, int comissao) {
-        return ( this.salarioBase + horasExtras + comissao);
+    
+    public double calcularFerias () {
+        return this.salarioBase + this.salarioBase/3;
     }
-
-    public boolean contabilizarVenda(double venda) {
+    
+    public double salarioDoMes () {
+        return this.salarioBase + this.horaExtra + this.calcularComissao();
+    }
+    
+    public void exibirResumo () {
+        System.out.println("Nome: " + this.nomeCompleto);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Registro: " + this.registro);
+        System.out.println("Salário total: " + this.salarioDoMes());
+        System.out.println("Comissão: " + this.calcularComissao());
+        System.out.println("Férias: " + this.calcularFerias());
+        System.out.println("13º: " + this.calcularDecimoTerceiro());
+        System.out.println("Vendas: " + this.totalVendas);
+    }
+    
+    public boolean contabilizarVenda (double venda) {
         if (venda > 0) {
             this.totalVendas = this.totalVendas + venda;
             return true;
@@ -94,15 +113,4 @@ public class Vendedor {
             return false;
         }
     }
-    
-    public void vendedor (String nome, String cpf, String sobrenome, int registro, int salario, Gerente chefe){
-        this.setNome(nome);
-        this.setSobrenome(sobrenome);
-        this.setCpf(cpf);
-        this.setSalario(salario);
-        this.setRegistro(registro);
-        this.gerente = chefe;
-        
-    }
-    
 }
