8f47d32205eee18aeac37d2d78481c6941d760a6
Pedro Cattel <pedrocattel@hotmail.com>
2017-08-08 11:10:44 -0300

    interface

---
+++ pratica3/AtividadeSimulação/src/atividadesimulação/AtividadeSimulação.java
@@ -0,0 +1,17 @@
+
+package atividadesimulação;
+
+/**
+ *
+ * @author pedrocattel
+ */
+public class AtividadeSimulação {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
